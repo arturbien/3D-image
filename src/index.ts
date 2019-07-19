5f45c7ec46f361166157dda1571c1486a36d4aac
Artur Bien <artur.bien@gmail.com>
2019-07-20 00:01:41 +0200

    created customWindow interface

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-alert("💲");
+import { isString, isElement } from "./type_checking";
+import { Selector } from "./types";
+
+class Image3d {}
+export interface CustomWindow extends Window {
+  image3d: any;
+}
+declare let window: CustomWindow;
+window.image3d = new Image3d();
