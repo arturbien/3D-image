62cb95303cd528865a326732935e77b0f2e2224b
Artur Bien <artur.bien@gmail.com>
2019-07-21 16:19:30 +0200

    fixed minor issues

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
     extensions: [".tsx", ".ts", ".js"]
   },
   output: {
-    filename: "bundle.js",
+    filename: "index.js",
     path: path.resolve(__dirname, "dist")
   }
 };
